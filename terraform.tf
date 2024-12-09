e4fd28ef267576cc7d26f59736fab6ab65c3c76f
lonegunmanb <lonegunmanb@hotmail.com>
2024-12-10 05:01:15 +0800

    fix: grept apply (#18)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.9.2"
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">=2.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "< 4.0.0"
@@ -9,17 +13,13 @@ terraform {
       source  = "azure/modtm"
       version = "~> 0.3"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = ">=2.0"
-    }
     null = {
       source  = "hashicorp/null"
       version = ">= 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
     }
   }
 }
