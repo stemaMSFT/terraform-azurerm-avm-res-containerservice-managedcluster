758088492f2377b8e3101f227a8be8fcbce15154
Saverio Proto <saverioproto@microsoft.com>
2025-01-28 11:02:27 +0100

    Let the user set some names if running outside of GitHub action

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -43,8 +43,8 @@ module "naming" {
 
 # This is required for resource modules
 resource "azurerm_resource_group" "this" {
-  location = module.regions.regions[random_integer.region_index.result].name
-  name     = module.naming.resource_group.name_unique
+  location = coalesce(var.location, module.regions.regions[random_integer.region_index.result].name)
+  name     = coalesce(var.resource_group_name, module.naming.resource_group.name_unique)
 }
 
 data "azurerm_client_config" "current" {}
@@ -55,7 +55,7 @@ data "azurerm_client_config" "current" {}
 # with a data source.
 module "default" {
   source              = "../.."
-  name                = module.naming.kubernetes_cluster.name_unique
+  name                = coalesce(var.cluster_name, module.naming.kubernetes_cluster.name_unique)
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 
